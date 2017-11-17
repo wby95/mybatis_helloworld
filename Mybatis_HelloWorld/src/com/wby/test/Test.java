6a1f3b46d01110d0384edeb3d50a5ab5e6c204c1
wby95 <870701494@qq.com>
2017-11-17 14:15:08 +0800

    mybatis的简单应用

--- Mybatis_HelloWorld/src/com/wby/test/Test.java
+++ Mybatis_HelloWorld/src/com/wby/test/Test.java
@@ -8,34 +8,34 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+
 import com.wby.dao.UserMapper;
 import com.wby.entities.User;
-
-
-
-
 public class Test {
-
 	public static void main(String[] args)  {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream;
+		
 		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
+			//下面四行是获取 SqlSessionFactory的方式,其目的是为了获取 SqlSession
+			 String resource = "mybatis-config.xml";
+			 InputStream inputStream;
+			 inputStream = Resources.getResourceAsStream(resource);
+			 SqlSessionFactory sqlSessionFactory =new SqlSessionFactoryBuilder().build(inputStream);
+			 /*  SqlSession 是非线程安全的，所以不能把它设成一个成型变量
+			  *  SqlSession 代表与数据库的一次会话，相当于Connection
+			  *  SqlSession 使用完成后记得关闭
+			  * */
 			 SqlSession session=sqlSessionFactory.openSession();
+			 //获取dao接口的代理类
 			 UserMapper userMapper=session.getMapper(UserMapper.class);
+		
 			 User user =  userMapper.getUserById(1);
 			 System.out.println(user);
 			 session.close();
 			 } 
-		catch (IOException e) {
+		    catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		 
-		 
+		} 
 	}
 }
 	
